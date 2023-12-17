9c765f7d8349b222b77ccd145d53f9c944d06145
Nikola <82724411+nikola-tbilisski@users.noreply.github.com>
2023-12-17 17:05:10 +0400

    EmplChat initial commit

---
+++ EmplChat/EmplChat/src/main/java/com/kvantino/EmplChat/EmplChatApplication.java
@@ -0,0 +1,13 @@
+package com.kvantino.EmplChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplChatApplication.class, args);
+	}
+
+}
