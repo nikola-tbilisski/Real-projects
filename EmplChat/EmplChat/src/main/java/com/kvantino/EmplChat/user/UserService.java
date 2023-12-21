26bfd03735e6b8d33ac7b6f73b94985fb17a5005
Nikola <82724411+nikola-tbilisski@users.noreply.github.com>
2023-12-21 14:01:04 +0400

    EmplChat user package commit

---
+++ EmplChat/EmplChat/src/main/java/com/kvantino/EmplChat/user/UserService.java
@@ -0,0 +1,18 @@
+package com.kvantino.EmplChat.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
