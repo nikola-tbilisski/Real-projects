008575d43e882956b20a45d67d9edcd31692d12f
Nikola <82724411+nikola-tbilisski@users.noreply.github.com>
2024-06-27 15:41:16 +0400

    netnodes Server lombok refactoring and frontend developing commit

--- NetNodesApp/netnodefrontapp/src/app/service/server.service.ts
+++ NetNodesApp/netnodefrontapp/src/app/service/server.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, tap, throwError } from 'rxjs';
 import { CustomResponse } from '../interface/custom-response';
+import { Server } from '../interface/server';
+import { Status } from '../enum/status.enum';
 
 @Injectable({ providedIn: 'root' })
 export class ServerService {
@@ -9,20 +11,68 @@ export class ServerService {
 
   constructor(private http: HttpClient) { }
 
-  // simple procedural request to backend
-  getServers(): Observable<CustomResponse> {
-    return this.http.get<CustomResponse>('http://localhost:8080/server/list')
-  }
+  // // simple procedural request to backend
+  // getServers(): Observable<CustomResponse> {
+  //   return this.http.get<CustomResponse>('http://localhost:8080/server/list')
+  // }
 
-  //reactive approach of request to beckend
+  //typescript reactive approach of request to beckend
   servers$ = <Observable<CustomResponse>>
-  this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
-  .pipe(
-    tap(console.log),
-    catchError(this.handleError)
-  );
+    this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
 
-  handleError(handleError: any): Observable<never> {
-    return throwError (() => new Error('Method not implemented.'));
+  save$ = (server: Server) => <Observable<CustomResponse>>
+    this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  ping$ = (ipAddress: string) => <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/server/ping/${ipAddress}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+    new Observable<CustomResponse>(
+      subscriber => {
+        console.log(response);
+        subscriber.next(
+          status === Status.ALL ? { ...response, message: `Servers filtered by ${status} status` } :
+            {
+              ...response,
+              message: response.data.servers
+                .filter(server => server.status === status).length > 0 ? `Servers filtered by
+          ${status === Status.SERVER_UP ? 'Server Up'
+                : 'Server Down'} status` : `No servers of ${status} found`,
+              data: {
+                servers: response.data.servers
+                  ?.filter(server => server.status === status)
+              }
+            }
+        );
+        subscriber.complete;
+      }
+    )
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  delete$ = (serverId: number) => <Observable<CustomResponse>>
+    this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete/${serverId}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError(() => new Error(`An error occurred - Error code: ${error.status}`));
   }
 }
--- NetNodesApp/networknode/src/main/java/com/kvantino/networknode/model/Server.java
+++ NetNodesApp/networknode/src/main/java/com/kvantino/networknode/model/Server.java
@@ -3,12 +3,16 @@ package com.kvantino.networknode.model;
 import com.kvantino.networknode.enumaration.Status;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "server")
@@ -36,4 +40,20 @@ public class Server {
 
     @Column(name = "status")
     private Status status;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Server server = (Server) o;
+        return getId() != null && Objects.equals(getId(), server.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
