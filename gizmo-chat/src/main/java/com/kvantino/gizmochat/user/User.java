d5d6fa7668515d0dac80de3baf69584ac6e0247d
Nikola <82724411+nikola-tbilisski@users.noreply.github.com>
2025-04-05 18:26:10 +0400

    gizmo-chat first commit

---
+++ gizmo-chat/src/main/java/com/kvantino/gizmochat/user/User.java
@@ -0,0 +1,41 @@
+package com.kvantino.gizmochat.user;
+
+import com.kvantino.gizmochat.chat.Chat;
+import com.kvantino.gizmochat.common.BaseAuditingEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseAuditingEntity {
+
+    private static final int LAST_ACTIVE_INTERVAL = 5;
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime lastSeen;
+
+    @OneToMany(mappedBy = "sender")
+    private List<Chat> chatsAsSender;
+
+    @OneToMany(mappedBy = "recipient")
+    private List<Chat> chatsAsRecipient;
+
+    @Transient
+    public boolean isUserOnline() {
+        return lastSeen != null && lastSeen.isAfter(LocalDateTime.now().minusMinutes(LAST_ACTIVE_INTERVAL));
+    }
+}
---
+++ gizmo-chat/src/test/java/com/kvantino/gizmochat/GizmoChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvantino.gizmochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GizmoChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
